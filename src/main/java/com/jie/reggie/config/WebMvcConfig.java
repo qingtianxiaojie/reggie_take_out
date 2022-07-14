02f314c7d5e60c5a41ad911cee54eec55af5b1a8
17597 <1759747247@qq.com>
2022-07-14 15:16:59 +0800

    2022年7月14日15点16分

--- src/main/java/com/jie/reggie/config/WebMvcConfig.java
+++ src/main/java/com/jie/reggie/config/WebMvcConfig.java
@@ -9,7 +9,6 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.util.List;
 
 @Slf4j
